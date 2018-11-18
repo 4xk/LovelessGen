3900bbeedf364ea6eea2fb274206485194ef475b
LovelessCodes <45143926+LovelessCodes@users.noreply.github.com>
2018-11-18 14:45:15 +0100

    Adding extractor

---
+++ modules/extractor.py
@@ -0,0 +1,26 @@
+import ctypes
+import re
+import os
+import sys
+def extract():
+    extracts = 0
+    clear = lambda: os.system('cls')
+    clear()
+    ctypes.windll.kernel32.SetConsoleTitleW("Loveless.codes | Google Mini Extractor")
+    if not os.path.exists("extracting.txt"):
+        open("extracting.txt", mode='x').close()
+        input('Please paste all your codes as such "link" in extracting.txt\r\n')
+        clear()
+        sys.exit(0)
+    with open("extracting.txt", "r") as extract:
+        for link in extract:
+            b = re.search('.*promoCode=(.*?)$',link)
+            extracted = b.group(1)
+            print(extracted)
+            with open("codes.txt", "a") as saveCode:
+                saveCode.write(extracted + "\n")
+                extracts += 1
+    ctypes.windll.kernel32.SetConsoleTitleW("Finished! | Extracted: [" + str(extracts) + "]")
+    print("\nExtracted: " + str(extracts) + "\r")
+    print("Done \o/\r")
+    input("Do you want to restart?\r\n")
