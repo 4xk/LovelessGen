50a8cf9fc8f98024a79867e0cbb193754ffa1d99
LovelessCodes <45143926+LovelessCodes@users.noreply.github.com>
2018-11-18 14:45:12 +0100

    Adding extractor

--- gen.py
+++ gen.py
@@ -1,12 +1,14 @@
-from modules import codegen
+from modules import codegen, extractor
 import ctypes
 import os
 while True:
     clear = lambda: os.system('cls')
     clear()
     ctypes.windll.kernel32.SetConsoleTitleW("Loveless.codes | Discord: https://discord.gg/pWz6Mb8")
-    module2load = input("Welcome to Loveless!\r\nWhich module would you like to load?\r\n\r\n1. Google Mini Code Gen\r\n\r\n")
+    module2load = input("Welcome to Loveless!\r\nWhich module would you like to load?\r\n\r\n1. Google Mini Code Gen\r\n2. Google Mini Link Extractor\r\n\r\n")
     if "1" in module2load:
         codegen.generate()
+    if "2" in module2load:
+        extractor.extract()
     else:
         clear()
