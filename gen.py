e4b44315f50ac9e1fce2fdbbdbb8a13cb4af9d7a
LovelessCodes <45143926+LovelessCodes@users.noreply.github.com>
2018-11-18 14:01:38 +0100

    Added normal files, k?

---
+++ gen.py
@@ -0,0 +1,14 @@
+from modules import codegen
+import random
+import ctypes
+import re
+import os
+while True:
+    clear = lambda: os.system('cls')
+    clear()
+    ctypes.windll.kernel32.SetConsoleTitleW("Loveless.codes | Discord: https://discord.gg/pWz6Mb8")
+    module2load = input("Welcome to Loveless!\r\nWhich module would you like to load?\r\n\r\n1. Google Mini Code Gen\r\n\r\n")
+    if "1" in module2load:
+        codegen.generate()
+    else:
+        clear()
---
+++ modules/codegen.py
@@ -0,0 +1,27 @@
+import random
+import ctypes
+import re
+import os
+def generate():
+    hitcounter = 0
+    generated = 0
+    letters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWZXY'
+    clear = lambda: os.system('cls')
+    clear()
+    ctypes.windll.kernel32.SetConsoleTitleW("Loveless.codes | Random Google Mini Code Gen")
+    amount = input('How many codes do you want to generate?\r\n')
+    clear()
+    for x in range(int(amount)):
+        generatedCode = 'B-';
+        remaining = int(amount) - x;
+        ctypes.windll.kernel32.SetConsoleTitleW("Generated: [" + str(generated) + "] | Remaining: [" + str(remaining) + "]")
+        for i in range(23):
+            generatedCode += random.choice(letters)
+        print(generatedCode)
+        with open("codes.txt", "a") as saveCode:
+            saveCode.write(generatedCode + "\n")
+            generated += 1
+    ctypes.windll.kernel32.SetConsoleTitleW("Finished! | Generated: [" + str(generated) + "]")
+    print("Generated: " + str(generated) + "\r")
+    print("Done \o/\r")
+    input("Do you want to restart?\r\n")
